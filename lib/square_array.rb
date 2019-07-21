13a173c2eea260c9dc049397b70dc32cb5080b43
Jinai Franklin <jinai.franklin@yahoo.com>
2019-07-21 07:29:09 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def square_array(array) #[1,2,3]
   count = 0
   new_array = []
@@ -11,3 +12,16 @@ def square_array(array) #[1,2,3]
   return new_array #[1,4,9]
 end
 
+=======
+def square_array(array)
+  count = 0
+  new_array = []
+  while count < array.length
+  
+    new_array << 1
+    count += 1
+  end
+  
+  return new_array
+end
+>>>>>>> 8e77adb52417d67556dd34aa489c863475160b24
